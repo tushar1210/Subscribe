1c671247b8195b73551e047270d6d5817ae79cdc
tushar.1210 <ts26192271@gmail.com>
2020-09-21 21:46:38 +0530

    add basecase handling

--- Subscribe/View/CardView.swift
+++ Subscribe/View/CardView.swift
@@ -18,33 +18,24 @@ class CardView:UIView{
     var dateLabel = UILabel()
     var nameLabel = UILabel()
     
-    enum cardProviderImageEnum:String {
-        case masterCard = "MasterCardLogo"
-        case visa = "VisaLogo"
-        case amex = "AmericanExpressLogo"
-        case rupay = "RuPayLogo"
-    }
-    
-    enum CardTypeEnum:String {
-        case debit = "DEBIT"
-        case credit = "CREDIT"
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    init(_ frame:CGRect = CGRect(x: 0, y: 0, width: 323, height: 170)) {
+    init(_ frame:CGRect = CGRect(x: 0, y: 0, width: 323, height: 170),name:String="Card Holder", cardNumber:String="", date:String="MM/YY", vendor:CardModel.cardProviderImageEnum = .masterCard, type:CardModel.typeEnum = .credit) {
         super.init(frame:frame)
         self.frame = frame
         self.cardModel.frame = frame
+        self.cardModel.name = name
+        self.cardModel.cardNumber=cardNumber
+        self.cardModel.date=date
+        self.cardModel.cardImage = vendor
+        self.cardModel.type = type
         self.generateGradient()
         self.setUpView()
         self.layer.cornerRadius = 10
         self.addSublayouts()
     }
-    
-
     
     private func generateGradient(){
         let gradient = CAGradientLayer()
@@ -66,13 +57,13 @@ class CardView:UIView{
     
     private func addSublayouts(){
         logoImageView.frame = CGRect(x: frame.minX+8, y: frame.minY+5, width: 75, height: 30)
-        logoImageView.image = UIImage(named: cardProviderImageEnum.visa.rawValue)
+        logoImageView.image = UIImage(named: cardModel.cardImage!.rawValue)
         logoImageView.contentMode = .scaleAspectFit
         addSubview(logoImageView)
         
         typeLabel.frame = CGRect(x: frame.midX-35, y: 30, width: 70, height: 20)
         typeLabel.center = CGPoint(x: frame.midX, y: 30)
-        typeLabel.attributedText = NSAttributedString(string: "DEBIT", attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
+        typeLabel.attributedText = NSAttributedString(string: cardModel.type!.rawValue, attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
         typeLabel.textAlignment = .center
         typeLabel.font = UIFont(name: "AndaleMono", size: 18)
         typeLabel.adjustsFontSizeToFitWidth = true
@@ -80,61 +71,49 @@ class CardView:UIView{
         
         cardNumberLabel.frame = CGRect(x: frame.midX-145 , y: 83, width: 290, height: 25)
         cardNumberLabel.center = CGPoint(x: frame.midX, y: 83)
-        cardNumberLabel.text = "XXXX  XXXX  XXXX  XXXX"
+        cardNumberLabel.text = setCardNumber(number: cardModel.cardNumber!)
         cardNumberLabel.textAlignment = .center
         cardNumberLabel.font = UIFont(name: "AndaleMono", size: 22)
         cardNumberLabel.adjustsFontSizeToFitWidth = true
         addSubview(cardNumberLabel)
         
         dateLabel.frame = CGRect(x: frame.minX + 15, y: frame.maxY-29, width: 45, height: 17)
-        dateLabel.text = "MM/YY"
+        dateLabel.text = cardModel.date
         dateLabel.textAlignment = .center
         dateLabel.font = UIFont(name: "AndaleMono", size: 15)
         dateLabel.adjustsFontSizeToFitWidth = true
         addSubview(dateLabel)
         
         nameLabel.frame = CGRect(x: dateLabel.frame.maxX, y: frame.maxY-29, width: cardNumberLabel.frame.width-dateLabel.frame.width, height: 17)
-        nameLabel.text = "Card Holder"
+        nameLabel.text = cardModel.name
         nameLabel.textAlignment = .right
         nameLabel.font = UIFont(name: "AndaleMono", size: 15)
         nameLabel.adjustsFontSizeToFitWidth = true
         addSubview(nameLabel)
-        
-    }
-
-    public func setLogo(logo: cardProviderImageEnum){
-        logoImageView.image = UIImage(named: "home")
     }
     
-    public func setType(type:CardTypeEnum){
-        typeLabel.attributedText = NSAttributedString(string: type.rawValue, attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
-    }
-    
-    public func setCardNumber(number:String){
-        var formattedString = String()
-        var ctr=0
-        for i in number{
-            if(ctr==4){
-                formattedString += "  "
-                ctr=0
-            }
-            formattedString.append(i)
-            ctr+=1
+    public func setCardNumber(number:String)->String{
+        if(number=="" || number.count==0){
+            return "••••   ••••   ••••   ••••"
         }
-        if(number.count<16){
-            let diff=16-number.count-1
-            for _ in 0...diff{
-                if(ctr==4){
-                    formattedString += "  "
-                    ctr=0
-                }
-                formattedString.append("X")
-                ctr+=1
-            }
+        var formated = ""
+        let dif=16-number.count
+        var ct=0
+        for _ in 0...dif{
+            formated.append("•")
         }
-        cardNumberLabel.text = formattedString
+        formated.removeLast()
+        formated+=number
+        var main=""
+        for i in formated{
+            if(ct==4){
+                main+="   "
+                ct=0
+            }
+            main.append(i)
+            ct+=1
+        }
+        return main
     }
-    
-
-    
+       
 }
